69e42c65415af8eef1aa380f146de36a860ba0d4
Evan Schoenberg <>
2020-01-01 14:50:58 -0500

    Fix nullability warnings; alert title and message are nullable, while the rest of the parameters are not.

--- Classes/SCPStoreKitReceiptValidator/SCPStoreKitReceiptValidator.h
+++ Classes/SCPStoreKitReceiptValidator/SCPStoreKitReceiptValidator.h
@@ -11,6 +11,8 @@
 
 #import "SCPStoreKitReceipt.h"
 
+NS_ASSUME_NONNULL_BEGIN
+
 typedef void(^Success)(SCPStoreKitReceipt *receipt);
 typedef void(^Failure)(NSError *error);
 
@@ -18,8 +20,10 @@ typedef void(^Failure)(NSError *error);
 
 + (nonnull instancetype)sharedInstance;
 
-- (void)validateReceiptWithBundleIdentifier:(NSString *)bundleIdentifier bundleVersion:(NSString *)bundleVersion tryAgain:(BOOL)tryAgain showReceiptAlert:(BOOL)showReceiptAlert alertViewTitle:(NSString *)alertViewTitle alertViewMessage:(NSString *)alertViewMessage success:(Success)successBlock failure:(Failure)failureBlock;
+- (void)validateReceiptWithBundleIdentifier:(NSString *)bundleIdentifier bundleVersion:(NSString *)bundleVersion tryAgain:(BOOL)tryAgain showReceiptAlert:(BOOL)showReceiptAlert alertViewTitle:(nullable NSString *)alertViewTitle alertViewMessage:(nullable NSString *)alertViewMessage success:(Success)successBlock failure:(Failure)failureBlock;
 
-- (void)validateReceiptWithBundleIdentifier:(NSString *)bundleIdentifier bundleVersion:(NSString *)bundleVersion acceptOtherVersions:(BOOL)looseVersionChecking tryAgain:(BOOL)tryAgain showReceiptAlert:(BOOL)showReceiptAlert alertViewTitle:(NSString *)alertViewTitle alertViewMessage:(NSString *)alertViewMessage success:(Success)successBlock failure:(Failure)failureBlock;
+- (void)validateReceiptWithBundleIdentifier:(NSString *)bundleIdentifier bundleVersion:(NSString *)bundleVersion acceptOtherVersions:(BOOL)looseVersionChecking tryAgain:(BOOL)tryAgain showReceiptAlert:(BOOL)showReceiptAlert alertViewTitle:(nullable NSString *)alertViewTitle alertViewMessage:(nullable NSString *)alertViewMessage success:(Success)successBlock failure:(Failure)failureBlock;
+
+NS_ASSUME_NONNULL_END
 
 @end
